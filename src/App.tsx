ead24e022dd51b92dadc82a990f8de43e645ee0d
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-08-22 16:36:24 +0800

    feat: add time travel

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,6 @@ function Square(props:any) {
   );
 }
 
-function calculateWinner(squares: []) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 class Board extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -69,6 +49,27 @@ class Board extends React.Component<any, any> {
   }
 }
 
+
+function calculateWinner(squares: []) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 class Game extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -78,12 +79,13 @@ class Game extends React.Component<any, any> {
           squares: Array(9).fill(null)
         }
       ],
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
   handleClick(i:number) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -92,17 +94,21 @@ class Game extends React.Component<any, any> {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: [...history, {squares}],
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
 
-  jumpTo(move: number) {
-
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     let status;
