f51eecfd65e5639bf9d4f8c85fe34e9849df4760
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-08-22 14:23:52 +0800

    feat: add taking turn

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,18 @@ class Board extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i:number) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i:number) {
@@ -36,7 +40,7 @@ class Board extends React.Component<any, any> {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
